43dfd922a0541c90f11a73a06c7b9f2e76d7fb8e
ASI <asi@dpaw.wa.gov.au>
2016-02-02 08:55:02 +0800

    Correct wsgi.py (confy env setting must precede dj_static import).

--- ibms_project/wsgi.py
+++ ibms_project/wsgi.py
@@ -3,9 +3,9 @@ WSGI config for IBMS project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 """
 import confy
+confy.read_environment_file('.env')  # Must precede dj_static imports.
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 
-confy.read_environment_file()
 application = Cling(get_wsgi_application())
