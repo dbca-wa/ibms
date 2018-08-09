fb89b95f4c8b9d22f9832f6d1c646277d4f237c8
Ashley Felton <ashley@ropable.com>
2018-08-09 08:25:43 +0800

    Update Django to 2.1, fix unit tests to suit.

--- ibms_project/settings.py
+++ ibms_project/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 ]
 SITE_TITLE = 'Integrated Business Management System'
 SITE_ACRONYM = 'IBMS'
-APPLICATION_VERSION_NO = '2.3.4'
+APPLICATION_VERSION_NO = '2.4'
 ADMINS = ('asi@dbca.wa.gov.au',)
 MANAGERS = (
     ('Zen Wee', 'zen.wee@dbca.wa.gov.au', '9219 9928'),
