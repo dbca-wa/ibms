7ab3068a70cad4a8767960a0ca68f7b4081cba89
Ashley Felton <ashley@ropable.com>
2022-10-14 08:37:54 +0800

    Better site titles in Django admin.

--- ibms_project/urls.py
+++ ibms_project/urls.py
@@ -3,6 +3,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from ibms.views import SiteHomeView, HealthCheckView
 
+
+admin.site.site_header = 'IBMS database administration'
+admin.site.index_title = 'IBMS database'
+admin.site.site_title = 'IBMS'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
